e9f906a00d3884d465874ba7edf218a88a5fe138
WaslleyNanferdes <waslleynanferdes.dev@gmail.com>
2024-10-07 22:07:25 -0300

    Arquivo produtos.csv adicionado, base de dados disponibilizado pela hashtag treinamentos, código finalizado com a função cadastro_produtos, embora eu não saiba o por que de na linha 23, o conteudo de send_keys TEM que ser string, porque se não dará erro, um TypeError: 'numpy.int64' object is not iterable

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
-from time import sleep
-from random import randint
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
-def open_browser(driver, url : str):
+def open_browser(driver, url):
     driver.get(url)
     driver.maximize_window()
 
 def login(driver, wait = 30):
     driver.find_element(By.ID, 'email').send_keys('testeautomacao12345654321@teste.com')
     driver.implicitly_wait(wait)
-    sleep(randint(1, 3))
     driver.find_element(By.ID, 'password').send_keys('testeautomacao12345654321')
     driver.implicitly_wait(wait)
-    sleep(randint(1, 3))
     driver.find_element(By.ID, 'pgtpy-botao').click()
     driver.implicitly_wait(wait)
+
+def cadastro_produtos(driver, ids, sheet, wait = 30):
+    for i in sheet.index:
+        for j, k in enumerate(sheet.columns):
+            if str(sheet[k][i]) == 'nan':
+                pass
+            else:
+                driver.find_element(By.ID, ids[j]).send_keys(str(sheet[k][i]))
+                driver.implicitly_wait(wait)
+        driver.find_element(By.ID, 'pgtpy-botao').click()
+        driver.implicitly_wait(wait)
 
 def main():
     url = 'https://dlp.hashtagtreinamentos.com/python/intensivao/login'
     driver = webdriver.Chrome()
+    csv = pd.read_csv('./produtos.csv')
+    ids = ['codigo', 'marca', 'tipo', 'categoria', 'preco_unitario', 'custo', 'obs']
     
     open_browser(driver, url)
     login(driver)
+    cadastro_produtos(driver, ids, csv)
 
 if __name__ == "__main__":
     main()
